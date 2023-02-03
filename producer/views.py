78152ebb5a842f68b1e08bd9e9fee2c83ce59f72
Povladislav <vladpomalejko@gmail.com>
2023-02-03 16:19:22 +0300

    fix headers

--- producer/views.py
+++ producer/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
 from producer.models import Producer
--- showroom/views.py
+++ showroom/views.py
@@ -1,7 +1,8 @@
-from rest_framework.permissions import IsAdminUser, AllowAny
-from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from rest_framework import filters
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.viewsets import ModelViewSet
+
 from showroom.models import ShowRoom
 from showroom.serializers import ShowRoomSerializer
 
