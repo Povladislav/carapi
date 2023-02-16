2bc85055c68f35dc29257e23f7d156102ac81af3
Povladislav <vladpomalejko@gmail.com>
2023-02-16 16:02:49 +0300

    Registration update

--- api/urls.py
+++ api/urls.py
@@ -19,7 +19,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+                                            TokenRefreshView, TokenVerifyView)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,8 +36,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/', include('car.urls')),
     path('api/v1/', include('producer.urls')),
     path('api/v1/', include('showroom.urls')),
