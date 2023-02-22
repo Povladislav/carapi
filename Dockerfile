37860bbb0a5d6bb0bd7f84c9bb84d038f4bb5a70
Povladislav <vladpomalejko@gmail.com>
2023-02-22 12:51:40 +0300

    added gunicorn + nginx

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,6 @@ WORKDIR /code
 COPY Pipfile Pipfile.lock /code/
 RUN pip install pipenv && pipenv install --dev --system --deploy
 COPY . /code/
+
+COPY ./entrypoint.sh /
+ENTRYPOINT ["sh","/entrypoint.sh"]
--- api/urls.py
+++ api/urls.py
@@ -21,6 +21,9 @@ from rest_framework import permissions
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
@@ -47,4 +50,4 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
