aa4849744e9e05fa7c791431352565ff5c4b50f7
Povladislav <vladpomalejko@gmail.com>
2023-02-27 16:43:47 +0300

    manage.py command

---
+++ customer/management/commands/populate_db.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from scripts.populate_db import *
+
+
+class Command(BaseCommand):
+    help = 'Populating our DBase'
+
+    # def add_arguments(self, parser):
+    #     ...
+
+    def handle(self, *args, **options):
+        populate_with_sql()
+        populate_cars()
+        populate_with_locations()
+        populate_with_showrooms()
+        populate_with_producers()
+        populate_with_av_cars()
